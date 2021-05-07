50c822ecbc44ccf7b736f2df8dd1342de427fd0f
TwiliShiba <twilishiba@gmail.com>
2021-05-06 23:45:28 -0500

    Tere Angulo | Fantasmas más inteligentes, nombres en parte superior, colores de fantasmas

---
+++ pacman.py
@@ -0,0 +1,208 @@
+# Tere Angulo
+# Andrés Yarte
+"Reflexión"
+"Sin duda, este ha sido el programa más complejo que hemos hecho hasta ahora, debido a que "
+"debía de implementarse inteligencia para que los fantasmas fueran hacia pacman al detectarlo."
+"Ya que el uso algoritmos de búsqueda de camino más corto y grafos hubiera sido muy complicado,"
+"optamos por utilizar condiciones para regir el movimiento de los fantasmas."
+"A pesar de que no es 100% eficiente, se ha logrado que los fantasmas sigan a pacman cuando se"
+"encuentra en las mismas coordenadas x o y. Fue retador, pero aprendimos mucho"
+from random import choice
+from turtle import *
+from freegames import floor, vector
+
+state = {'score': 0}
+path = Turtle(visible=False)
+writer = Turtle(visible=False)
+wr = Turtle(visible=False)
+aim = vector(5, 0)
+pacman = vector(-40, -80)
+ghosts = [
+    [vector(-180, 160), vector(5, 0)],
+    [vector(-180, -160), vector(0, 5)],
+    [vector(100, 160), vector(0, -5)],
+    [vector(100, -160), vector(-5, 0)],
+]
+tiles = [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0,
+    0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+]
+
+def square(x, y):
+    "Draw square using path at (x, y)."
+    path.up()
+    path.goto(x, y)
+    path.down()
+    path.begin_fill()
+
+    for count in range(4):
+        path.forward(20)
+        path.left(90)
+
+    path.end_fill()
+
+def offset(point):
+    "Return offset of point in tiles."
+    x = (floor(point.x, 20) + 200) / 20
+    y = (180 - floor(point.y, 20)) / 20
+    index = int(x + y * 20)
+    return index
+
+def valid(point):
+    "Return True if point is valid in tiles."
+    index = offset(point)
+
+    if tiles[index] == 0:
+        return False
+
+    index = offset(point + 19)
+
+    if tiles[index] == 0:
+        return False
+
+    return point.x % 20 == 0 or point.y % 20 == 0
+
+def world():
+    "Draw world using path."
+    bgcolor('black')
+    path.color('blue')
+
+    for index in range(len(tiles)):
+        tile = tiles[index]
+
+        if tile > 0:
+            x = (index % 20) * 20 - 200
+            y = 180 - (index // 20) * 20
+            square(x, y)
+
+            if tile == 1:
+                path.up()
+                path.goto(x + 10, y + 10)
+                path.dot(2, 'white')
+
+def move():
+    def info_alumnos():
+        wr.up()
+        wr.goto(10,190)
+        wr.color('white')
+        wr.write('TereAnguloTello-A00825411', align='left', font=('Arial', 12, 'bold'))
+        wr.goto(-190,190)
+        wr.color('white')
+        wr.write('AndrésYarteVillarreal-A00829535', align='left', font=('Arial', 12, 'bold'))
+
+    info_alumnos()
+    "Move pacman and all ghosts."
+    writer.undo()
+    writer.write(state['score'])
+
+    clear()
+
+    if valid(pacman + aim):
+        pacman.move(aim)
+
+    index = offset(pacman)
+
+    if tiles[index] == 1:
+        tiles[index] = 2
+        state['score'] += 1
+        x = (index % 20) * 20 - 200
+        y = 180 - (index // 20) * 20
+        square(x, y)
+
+    up()
+    goto(pacman.x + 10, pacman.y + 10)
+    dot(20, 'yellow')
+
+    colores = ['red', 'green', 'cyan', 'white']
+    k = 0
+    for point, course in ghosts:
+        dist_x = point.x - pacman.x
+        dist_y = point.y - pacman.y
+        if (dist_y == 0):
+            if dist_x < 0:
+                course.x = 5
+                course.y = 0
+            elif dist_x > 0:
+                course.x = -5
+                course.y = 0
+        if (dist_x == 0):
+            if dist_y < 0:
+                course.x = 0
+                course.y = 5
+            elif dist_y > 0:
+                course.x = 0
+                course.y = -5
+        
+        if valid(point + course):
+            point.move(course)
+        else:
+            if course.x == 5 or course.x == -5:
+                op = [vector(0, -5), vector(0, 5)]
+                plan = choice(op)
+                course.x = plan.x
+                course.y = plan.y
+                if valid(point + course):
+                    point.move(course)
+            elif course.y == -5 or course.y == 5:
+                op = [vector(5, 0), vector(-5, 0)]
+                plan = choice(op)
+                course.x = plan.x
+                course.y = plan.y
+                if valid(point + course):
+                    point.move(course)
+
+        up()
+        goto(point.x + 10, point.y + 10)
+        dot(20, colores[k])
+        k = k + 1
+
+    update()
+
+    for point, course in ghosts:
+        if abs(pacman - point) < 20:
+            writer.goto(-120,-20)
+            writer.write('GAME OVER', font = ('Arial',15,'normal'))
+            return
+
+    update()
+
+    ontimer(move, 150)
+
+def change(x, y):
+    "Change pacman aim if valid."
+    if valid(pacman + vector(x, y)):
+        aim.x = x
+        aim.y = y
+
+setup(420, 420, 370, 0)
+hideturtle()
+tracer(False)
+writer.goto(160, 160)
+writer.color('white')
+writer.write(state['score'])
+listen()
+onkey(lambda: change(5, 0), 'Right')
+onkey(lambda: change(-5, 0), 'Left')
+onkey(lambda: change(0, 5), 'Up')
+onkey(lambda: change(0, -5), 'Down')
+world()
+move()
+done()
